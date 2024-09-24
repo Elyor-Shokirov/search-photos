bfaae394df0a2ed1e615eaac2b2c5edb861ddf66
Elyor Shokirov <elton08@list.ru>
2024-09-24 20:25:22 +0300

    first-commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+//read more button add
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
