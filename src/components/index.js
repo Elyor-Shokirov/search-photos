f53684df5f347273d247a83bdb3937504fa0e59d
Elyor Shokirov <elton08@list.ru>
2024-09-29 23:42:15 +0300

    complated unsplash

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ export { default as Badge } from "./badge";
 export { default as Footer } from "./footer.jsx";
 export { default as FormInput } from "./FormInput";
 export { default as Image } from "./Image";
+export { default as ImageContainer } from "./ImageContainer.jsx";
 export { default as Navbar } from "./Navbar";
 export { default as SearchBar } from "./searchBar";
--- src/pages/DownloadImages.jsx
+++ src/pages/DownloadImages.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import ImageContainer from "../components/imageContainer";
+import { ImageContainer } from "../components";
 import { useGlobalContext } from "../hooks/useGlobalContext";
 
 function DownloadImages() {
