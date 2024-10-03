341900b58a9ec03a252815f115969282f9fa12cd
Elyor Shokirov <elton08@list.ru>
2024-10-03 22:18:55 +0300

    Versioning untracked files

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as ForgatPassword } from "./ForgatPassword";
-export { default as Home } from "./Home";
-export { default as ImageInfo } from "./ImageInfo";
-export { default as LikedImages } from "./LikedImages";
-export { default as SignIn } from "./SignIn";
